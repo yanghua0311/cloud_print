2ec66a453cf872ddd6a15c6e17330a102d7640aa
youngwa <87965110@qq.com>
2019-01-09 22:57:54 +0800

    red: #0 @ 15m 添加项目结构

---
+++ src/main/java/com/cloud/print/CloudPrintApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPrintApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/cloud/print/model/ResponseBase.java
@@ -0,0 +1,7 @@
+package com.cloud.print.model;
+
+/**
+ * Created by youngwa on 2018/7/22.
+ */
+public interface ResponseBase {
+}
